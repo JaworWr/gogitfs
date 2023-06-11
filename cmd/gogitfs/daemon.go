7ecbc5e1a0efacf6ebf9faa61a54cfc6b12eea3e
Michał Jaworski <jawor403@gmail.com>
2023-06-11 20:02:24 +0200

    Mount sample file system

--- cmd/gogitfs/daemon.go
+++ cmd/gogitfs/daemon.go
@@ -1,27 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"github.com/hanwen/go-fuse/v2/fs"
 	"gogitfs/pkg/daemon"
 	"gogitfs/pkg/error_handler"
-	"time"
+	"gogitfs/pkg/gitfs"
+	"log"
+	"os"
 )
 
 type gogitfsDaemon struct{}
 
-func (g gogitfsDaemon) DaemonArgs(args []string) []string {
+func (g *gogitfsDaemon) DaemonArgs(args []string) []string {
+	return args
+}
+
+func (g *gogitfsDaemon) DaemonEnv(env []string) []string {
 	return nil
 }
 
-func (g gogitfsDaemon) DaemonEnv(env []string) []string {
-	return nil
-}
-
-func (g gogitfsDaemon) DaemonProcess(errHandler error_handler.ErrorHandler, succHandler daemon.SuccessHandler) {
-	fmt.Println("Hello from daemon")
-	time.Sleep(3 * time.Second)
-	fmt.Println("Bye")
+func (g *gogitfsDaemon) DaemonProcess(errHandler error_handler.ErrorHandler, succHandler daemon.SuccessHandler) {
+	mountDir := os.Args[1]
+	root := &gitfs.RootNode{}
+	log.Printf("Mounting in %v\n", mountDir)
+	server, err := fs.Mount(mountDir, root, &fs.Options{})
+	if err != nil {
+		errHandler.HandleError(err)
+	}
 	succHandler.HandleSuccess()
+	server.Wait()
 }
 
 var _ daemon.ProcessInfo = (*gogitfsDaemon)(nil)
--- pkg/gitfs/root_node.go
+++ pkg/gitfs/root_node.go
@@ -1,4 +1,4 @@
-package fs
+package gitfs
 
 import (
 	"context"
