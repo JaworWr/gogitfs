c9b6264c39d79260353ad45576f6f57483158d02
Michał Jaworski <jawor403@gmail.com>
2023-06-11 21:29:09 +0200

    Error wrapper

---
+++ pkg/daemon/internal/error_handling/error_types.go
@@ -0,0 +1,31 @@
+package error_handling
+
+type subprocessError struct {
+	msg string
+}
+
+func (e *subprocessError) Error() string {
+	return e.msg
+}
+
+type subprocessErrorWrapper struct {
+	hasError bool
+	err      subprocessError
+}
+
+func wrapError(err error) *subprocessErrorWrapper {
+	res := subprocessErrorWrapper{hasError: false}
+	if err == nil {
+		return &res
+	}
+	res.hasError = true
+	res.err = subprocessError{err.Error()}
+	return &res
+}
+
+func (w *subprocessErrorWrapper) unwrap() error {
+	if !w.hasError {
+		return nil
+	}
+	return &w.err
+}
