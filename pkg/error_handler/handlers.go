7e1c21b1086c9ae75e2bd9353e783f7c8d644c68
Michał Jaworski <jawor403@gmail.com>
2023-06-11 22:51:09 +0200

    Remove unused error handlers

--- pkg/error_handler/handlers.go
+++ pkg/error_handler/handlers.go
@@ -8,22 +8,6 @@ type ErrorHandler interface {
 	HandleError(err error)
 }
 
-type noOpHandlerType struct{}
-
-func (h noOpHandlerType) HandleError(_ error) {
-
-}
-
-var NoOpHandler noOpHandlerType
-
-type fatalHandlerType struct{}
-
-func (h fatalHandlerType) HandleError(err error) {
-	log.Fatalf("[ERROR] An error occurred: %v", err.Error())
-}
-
-var FatalHandler fatalHandlerType
-
 type LogHandlerWrapper struct {
 	next ErrorHandler
 }
@@ -36,5 +20,3 @@ func (h *LogHandlerWrapper) HandleError(err error) {
 func MakeLoggingHandler(h ErrorHandler) *LogHandlerWrapper {
 	return &LogHandlerWrapper{next: h}
 }
-
-var LogHandler = &LogHandlerWrapper{NoOpHandler}
