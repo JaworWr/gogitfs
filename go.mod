4ab5230a16a243faef1e68c28843f31841f3fadb
Michał Jaworski <jawor403@gmail.com>
2023-06-11 19:29:01 +0200

    Sample file system

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module gogitfs
 
 go 1.20
 
-require github.com/sevlyar/go-daemon v0.1.6
+require (
+	github.com/hanwen/go-fuse/v2 v2.3.0
+	github.com/sevlyar/go-daemon v0.1.6
+)
 
 require (
 	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
---
+++ pkg/fs/root_node.go
@@ -0,0 +1,20 @@
+package fs
+
+import (
+	"context"
+	"github.com/hanwen/go-fuse/v2/fs"
+	"syscall"
+)
+
+type RootNode struct {
+	fs.Inode
+}
+
+func (n *RootNode) OnAdd(ctx context.Context) {
+	child := fs.MemRegularFile{}
+	child.Data = []byte("Hello there!")
+	childNode := n.NewPersistentInode(ctx, &child, fs.StableAttr{Mode: syscall.S_IFREG})
+	n.AddChild("hello.txt", childNode, false)
+}
+
+var _ fs.NodeOnAdder = (*RootNode)(nil)
