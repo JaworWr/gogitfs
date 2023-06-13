8ef6503b22c9bf8a2e93134d5ad8ba60b2d5d1d9
Michał Jaworski <jawor403@gmail.com>
2023-06-13 23:06:07 +0200

    Ino store

---
+++ pkg/inode_store/ino_store.go
@@ -0,0 +1,39 @@
+package inode_store
+
+import (
+	"fmt"
+	"github.com/hanwen/go-fuse/v2/fs"
+)
+
+type InoStore struct {
+	nextIno uint64
+	inos    map[string]uint64
+	gens    map[string]uint64
+}
+
+func NewInoStore(initialIno uint64) *InoStore {
+	store := InoStore{
+		nextIno: initialIno,
+		inos:    make(map[string]uint64),
+		gens:    make(map[string]uint64),
+	}
+	return &store
+}
+
+func (s *InoStore) GetOrInsert(hash fmt.Stringer, updateGen bool) fs.StableAttr {
+	hashStr := hash.String()
+	ino, ok := s.inos[hashStr]
+	if ok {
+		gen := s.gens[hashStr]
+		if updateGen {
+			gen += 1
+			s.gens[hashStr] += 1
+		}
+		return fs.StableAttr{Ino: ino, Gen: gen}
+	}
+	result := fs.StableAttr{Ino: s.nextIno, Gen: 0}
+	s.inos[hashStr] = s.nextIno
+	s.nextIno += 1
+	s.gens[hashStr] = 0
+	return result
+}
