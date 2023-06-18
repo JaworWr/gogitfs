7887035d6d061acaf75014999d77f163856a176b
Michał Jaworski <jawor403@gmail.com>
2023-06-19 00:39:49 +0200

    Implement OnAdd and GetAttr

--- pkg/gitfs/commit_node.go
+++ pkg/gitfs/commit_node.go
@@ -5,11 +5,27 @@ import (
 	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/hanwen/go-fuse/v2/fs"
 	"github.com/hanwen/go-fuse/v2/fuse"
+	"syscall"
 )
 
 type commitNode struct {
 	repoNode
 	commit object.Commit
+}
+
+func (n *commitNode) Getattr(_ context.Context, _ fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
+	out.Attr = n.attr()
+	return 0
+}
+
+func (n *commitNode) OnAdd(ctx context.Context) {
+	hashNode := &fs.MemRegularFile{Attr: n.attr(), Data: []byte(n.commit.Hash.String())}
+	child := n.NewPersistentInode(ctx, hashNode, fs.StableAttr{Mode: fuse.S_IFREG})
+	n.AddChild("hash", child, false)
+
+	msgNode := &fs.MemRegularFile{Attr: n.attr(), Data: []byte(n.commit.Message)}
+	child = n.NewPersistentInode(ctx, msgNode, fs.StableAttr{Mode: fuse.S_IFREG})
+	n.AddChild("message", child, false)
 }
 
 func newCommitNode(ctx context.Context, commit object.Commit, parent repoNodeEmbedder) *fs.Inode {
@@ -31,5 +47,5 @@ func (n *commitNode) attr() fuse.Attr {
 	}
 }
 
-//var _ fs.NodeOnAdder = (*commitNode)(nil)
-//var _ fs.NodeGetattrer = (*commitNode)(nil)
+var _ fs.NodeOnAdder = (*commitNode)(nil)
+var _ fs.NodeGetattrer = (*commitNode)(nil)
