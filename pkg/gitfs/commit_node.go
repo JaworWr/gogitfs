fc9704f0f41894b8d06143cab1912bafdd3d48ff
Michał Jaworski <jawor403@gmail.com>
2023-06-19 00:36:44 +0200

    Add attributes

--- pkg/gitfs/commit_node.go
+++ pkg/gitfs/commit_node.go
@@ -20,3 +20,16 @@ func newCommitNode(ctx context.Context, commit object.Commit, parent repoNodeEmb
 	}
 	return commitNodeMgr.GetOrInsert(ctx, commit.Hash, fuse.S_IFDIR, parent, builder, false)
 }
+
+func (n *commitNode) attr() fuse.Attr {
+	commitTime := (uint64)(n.commit.Author.When.Unix())
+	return fuse.Attr{
+		Mode:  0444,
+		Atime: commitTime,
+		Ctime: commitTime,
+		Mtime: commitTime,
+	}
+}
+
+//var _ fs.NodeOnAdder = (*commitNode)(nil)
+//var _ fs.NodeGetattrer = (*commitNode)(nil)
