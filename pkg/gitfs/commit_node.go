a47819d77a335da98753ec854e27f0ee18b80259
Michał Jaworski <jawor403@gmail.com>
2023-06-19 23:54:46 +0200

    Use correct permissions

--- pkg/gitfs/commit_node.go
+++ pkg/gitfs/commit_node.go
@@ -15,15 +15,18 @@ type commitNode struct {
 
 func (n *commitNode) Getattr(_ context.Context, _ fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
 	out.Attr = n.attr()
+	out.Mode = 0555
 	return 0
 }
 
 func (n *commitNode) OnAdd(ctx context.Context) {
-	hashNode := &fs.MemRegularFile{Attr: n.attr(), Data: []byte(n.commit.Hash.String())}
+	attr := n.attr()
+	attr.Mode = 0444
+	hashNode := &fs.MemRegularFile{Attr: attr, Data: []byte(n.commit.Hash.String())}
 	child := n.NewPersistentInode(ctx, hashNode, fs.StableAttr{Mode: fuse.S_IFREG})
 	n.AddChild("hash", child, false)
 
-	msgNode := &fs.MemRegularFile{Attr: n.attr(), Data: []byte(n.commit.Message)}
+	msgNode := &fs.MemRegularFile{Attr: attr, Data: []byte(n.commit.Message)}
 	child = n.NewPersistentInode(ctx, msgNode, fs.StableAttr{Mode: fuse.S_IFREG})
 	n.AddChild("message", child, false)
 }
@@ -40,7 +43,6 @@ func newCommitNode(ctx context.Context, commit *object.Commit, parent repoNodeEm
 func (n *commitNode) attr() fuse.Attr {
 	commitTime := (uint64)(n.commit.Author.When.Unix())
 	return fuse.Attr{
-		Mode:  0444,
 		Atime: commitTime,
 		Ctime: commitTime,
 		Mtime: commitTime,
