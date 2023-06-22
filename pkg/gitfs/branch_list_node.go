dc4bd68c4b9932a502b5be88cbdfaa63fd211272
Michał Jaworski <jawor403@gmail.com>
2023-06-23 01:11:44 +0200

    branchListNode skeleton

---
+++ pkg/gitfs/branch_list_node.go
@@ -0,0 +1,19 @@
+package gitfs
+
+import (
+	"github.com/go-git/go-git/v5"
+	"github.com/hanwen/go-fuse/v2/fs"
+)
+
+type branchListNode struct {
+	repoNode
+}
+
+func newBranchListNode(repo *git.Repository) *branchListNode {
+	node := &branchListNode{}
+	node.repo = repo
+	return node
+}
+
+var _ fs.NodeLookuper = (*branchListNode)(nil)
+var _ fs.NodeReaddirer = (*branchListNode)(nil)
