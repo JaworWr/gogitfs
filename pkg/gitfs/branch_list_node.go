f30095437c580513d7b8b5677cb03b5e7e5eeb9f
Michał Jaworski <jawor403@gmail.com>
2023-07-02 12:55:27 +0200

    Starrt implementing branchListNode

--- pkg/gitfs/branch_list_node.go
+++ pkg/gitfs/branch_list_node.go
@@ -3,6 +3,8 @@ package gitfs
 import (
 	"context"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/storer"
 	"github.com/hanwen/go-fuse/v2/fs"
 	"github.com/hanwen/go-fuse/v2/fuse"
 	"gogitfs/pkg/error_handler"
@@ -13,6 +15,42 @@ const BranchValid = 30
 
 type branchListNode struct {
 	repoNode
+}
+
+type branchDirStream struct {
+	next *plumbing.Reference
+	err  error
+	iter storer.ReferenceIter
+}
+
+func (s *branchDirStream) update() {
+
+}
+
+func (s *branchDirStream) HasNext() bool {
+	return s.next != nil
+}
+
+func (s *branchDirStream) Next() (entry fuse.DirEntry, errno syscall.Errno) {
+	if s.err != nil {
+		error_handler.Logging.HandleError(s.err)
+		errno = syscall.EIO
+		return
+	}
+	if s.next == nil {
+		errno = syscall.ENOENT
+		return
+	}
+	entry.Name = s.next.Name().Short()
+	//entry.Ino
+	entry.Mode = fuse.S_IFDIR
+	s.update()
+	return
+}
+
+func (s *branchDirStream) Close() {
+	s.next = nil
+	s.iter.Close()
 }
 
 func (n *branchListNode) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
