6a55f0e30ae6934d57a191b851b0b2d1fa4a4c50
Michał Jaworski <jawor403@gmail.com>
2023-07-21 20:37:07 +0200

    Per-level log objects

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -1,5 +1,12 @@
 package logging
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
 type LogLevelFlag int
 
 const (
@@ -9,4 +16,41 @@ const (
 	Error
 )
 
-var LogLevel LogLevelFlag = Info
+var logLevel = Info
+
+var DebugLog *log.Logger
+var InfoLog *log.Logger
+var WarningLog *log.Logger
+var ErrorLog *log.Logger
+
+func Init(l LogLevelFlag) {
+	logLevel = l
+	DebugLog = makeLogger(Debug)
+	InfoLog = makeLogger(Info)
+	WarningLog = makeLogger(Warning)
+	ErrorLog = makeLogger(Error)
+}
+
+func makeLogger(level LogLevelFlag) *log.Logger {
+	var output io.Writer
+	if level >= logLevel {
+		output = os.Stdout
+	} else {
+		output = io.Discard
+	}
+
+	var prefix string
+	switch level {
+	case Debug:
+		prefix = "DEBUG"
+	case Info:
+		prefix = "INFO"
+	case Warning:
+		prefix = "WARNING"
+	case Error:
+		prefix = "ERROR"
+	}
+	prefix = fmt.Sprintf("[%s] ", prefix)
+
+	return log.New(output, prefix, log.LstdFlags)
+}
