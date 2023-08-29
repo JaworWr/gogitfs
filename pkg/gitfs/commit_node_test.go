6cefdd2a7cddd21770d9e902776179dfa4739005
Michał Jaworski <jawor403@gmail.com>
2023-08-29 14:02:54 +0200

    Stat test

--- pkg/gitfs/commit_node_test.go
+++ pkg/gitfs/commit_node_test.go
@@ -2,6 +2,9 @@ package gitfs
 
 import (
 	"github.com/go-git/go-git/v5"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"path"
 	"testing"
 )
 
@@ -18,14 +21,25 @@ func commitNodeTestCase(t *testing.T, repo *git.Repository, extras repoExtras, c
 		_ = server.Unmount()
 	}()
 
-	var expected []string
+	var children []string
 	if hasParent {
-		expected = []string{"message", "hash", "log", "parent"}
+		children = []string{"message", "hash", "log", "parent"}
 	} else {
-		expected = []string{"message", "hash", "log"}
+		children = []string{"message", "hash", "log"}
 	}
 	t.Run("ls", func(t *testing.T) {
-		assertDirEntries(t, mountPath, expected)
+		assertDirEntries(t, mountPath, children)
+	})
+	t.Run("stat", func(t *testing.T) {
+		stat, err := os.Stat(mountPath)
+		assert.NoError(t, err, "unexpected error for commit node")
+		assert.Equal(t, commitSignatures[commit].When, stat.ModTime().UTC(), "incorrect time for commit node")
+
+		for _, c := range []string{"message", "hash"} {
+			stat, err := os.Stat(path.Join(mountPath, c))
+			assert.NoError(t, err, "unexpected error for %v", c)
+			assert.Equal(t, commitSignatures[commit].When, stat.ModTime().UTC(), "incorrect time for %v", c)
+		}
 	})
 }
 
