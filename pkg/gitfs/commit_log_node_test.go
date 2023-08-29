409c73b325a2fdce89bf2fc1aed27fb31ce74cdb
Michał Jaworski <jawor403@gmail.com>
2023-08-29 21:53:02 +0200

    Basepath test

--- pkg/gitfs/commit_log_node_test.go
+++ pkg/gitfs/commit_log_node_test.go
@@ -40,10 +40,43 @@ func Test_commitSymlink(t *testing.T) {
 			path := string(node.Data)
 			assert.Equal(t, tc.expectedPrefix+hash.String(), path, "incorrect path")
 
-			commitTime := uint64(commitSignatures[tc.commit].When.Unix())
-			assert.Equal(t, commitTime, node.Attr.Mtime, "incorrect mtime")
-			assert.Equal(t, commitTime, node.Attr.Atime, "incorrect atime")
-			assert.Equal(t, commitTime, node.Attr.Ctime, "incorrect ctime")
+			commitTime := commitSignatures[tc.commit].When.Unix()
+			assert.EqualValues(t, commitTime, node.Attr.Mtime, "incorrect mtime")
+			assert.EqualValues(t, commitTime, node.Attr.Atime, "incorrect atime")
+			assert.EqualValues(t, commitTime, node.Attr.Ctime, "incorrect ctime")
+		})
+	}
+}
+
+func Test_getBasePath(t *testing.T) {
+	tests := []struct {
+		name       string
+		linkLevels int
+		expectNil  bool
+		expected   string
+	}{
+		{
+			"zero",
+			0,
+			true,
+			"",
+		},
+		{
+			"non-zero",
+			2,
+			false,
+			"../..",
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			basePath := getBasePath(tc.linkLevels)
+			if tc.expectNil {
+				assert.Nil(t, basePath)
+			} else {
+				assert.NotNil(t, basePath)
+				assert.Equal(t, tc.expected, *basePath)
+			}
 		})
 	}
 }
