cc37a15ce4b0359ce473ec23d1ace8d222c561b3
Michał Jaworski <jawor403@gmail.com>
2023-10-01 00:13:30 +0200

    Basic mount test

--- test/gogitfs_tests/test_repo.py
+++ test/gogitfs_tests/test_repo.py
@@ -1,5 +1,22 @@
-from test.gogitfs_tests.conftest import RepoInfo
+import pathlib
+import subprocess
+
+import pytest
+import sh
+
+from test.gogitfs_tests.conftest import GOGITFS_BINARY
 
 
-def test_dummy(repo: RepoInfo):
-    assert repo.path.is_dir()
+@pytest.fixture
+def mount(repo_path: pathlib.Path, tmp_path: pathlib.Path):
+    args = [GOGITFS_BINARY, str(repo_path), str(tmp_path)]
+    p = subprocess.Popen(args)
+    p.wait()
+    assert p.returncode == 0
+    yield tmp_path
+    sh.umount(tmp_path)
+
+
+def test_mount_subdirs(mount: pathlib.Path):
+    assert (mount / "commits").is_dir()
+    assert (mount / "branches").is_dir()
