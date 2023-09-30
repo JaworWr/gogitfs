02d094c8e9b10c153d8018c897e6ce39550e46c7
Michał Jaworski <jawor403@gmail.com>
2023-09-30 13:40:00 +0200

    Remove clone call

--- test/gogitfs_tests/conftest.py
+++ test/gogitfs_tests/conftest.py
@@ -3,14 +3,11 @@ import pathlib
 import tempfile
 
 import pytest
-import sh
 
 GOGITFS_BINARY = os.environ["GOGITFS_BINARY"]
-REPO_URL = "<TODO>"
 
 
 @pytest.fixture(scope="session")
 def repo():
     with tempfile.TemporaryDirectory() as tmpdir:
-        sh.git.clone(REPO_URL, tmpdir)
         yield pathlib.Path(tmpdir)
