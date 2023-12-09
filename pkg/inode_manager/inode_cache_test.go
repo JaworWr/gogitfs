923c79235408e10522c65ecbf193c5d216964b33
Michał Jaworski <jawor403@gmail.com>
2023-12-09 19:52:21 +0100

    inode_store error messages

--- pkg/inode_manager/inode_cache_test.go
+++ pkg/inode_manager/inode_cache_test.go
@@ -64,8 +64,8 @@ func TestInodeCache_GetOrInsert(t *testing.T) {
 					return &fs.Inode{}, nil
 				}
 				result, err := cache.GetOrInsert(ctx, tc.key, tc.mode, root, builder, tc.overwrite)
-				assert.NoError(t, err)
-				assert.Equal(t, tc.expected, result.StableAttr())
+				assert.NoError(t, err, "unexpected error")
+				assert.Equal(t, tc.expected, result.StableAttr(), "incorrect node attributes")
 			})
 		}
 	})
