9fe1aace8a6cdbf9a2b9fe81bf670984408a365a
Michał Jaworski <jawor403@gmail.com>
2024-01-19 00:37:22 +0100

    Document all_commits_node.go

--- pkg/gitfs/all_commits_node.go
+++ pkg/gitfs/all_commits_node.go
@@ -16,8 +16,13 @@ import (
 	"time"
 )
 
+// HeadAttrValid represents validity time for HEAD symlink attributes
 const HeadAttrValid = 30 * time.Second
 
+// allCommitsNode implements a directory containing all commits in the repository.
+// Each commit is represented by a directory, whose name is the commit's hash.
+// Readdir and Lookup always consider the current state of the repository.
+// This way, new commits are included automatically.
 type allCommitsNode struct {
 	repoNode
 	headLink *fs.Inode
@@ -27,6 +32,8 @@ func (n *allCommitsNode) GetCallCtx() logging.CallCtx {
 	return utils.NodeCallCtx(n)
 }
 
+// headLinkNode implements the HEAD symlink, i.e. the symlink pointing to the most recent commit on the current branch.
+// Readlink always considers the current state of the repository.
 type headLinkNode struct {
 	repoNode
 }
@@ -42,6 +49,7 @@ func (n *headLinkNode) GetCallCtx() logging.CallCtx {
 	return info
 }
 
+// Readlink returns the path to the current HEAD commit.
 func (n *headLinkNode) Readlink(_ context.Context) ([]byte, syscall.Errno) {
 	logging.LogCall(n, nil)
 	head, err := n.repo.Head()
@@ -52,6 +60,7 @@ func (n *headLinkNode) Readlink(_ context.Context) ([]byte, syscall.Errno) {
 	return []byte(head.Hash().String()), fs.OK
 }
 
+// Getattr returns attributes corresponding to the current HEAD commit.
 func (n *headLinkNode) Getattr(_ context.Context, _ fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
 	logging.LogCall(n, nil)
 	attr, err := headAttr(n)
@@ -68,13 +77,22 @@ func (n *headLinkNode) Getattr(_ context.Context, _ fs.FileHandle, out *fuse.Att
 var _ fs.NodeReadlinker = (*headLinkNode)(nil)
 var _ fs.NodeGetattrer = (*headLinkNode)(nil)
 
+// commitDirStream implements an iterator over the contents of the directory representing all commits,
+// i.e. the HEAD symlink and the directories representing commits.
+// Uses a separate goroutine to actually read the directory.
 type commitDirStream struct {
+	// HEAD symlink
 	headLink *fs.Inode
-	next     *fuse.DirEntry
-	rest     <-chan *fuse.DirEntry
-	stop     chan<- int
+	// next entry to return
+	next *fuse.DirEntry
+	// channel returning remaining entries
+	rest <-chan *fuse.DirEntry
+	// channel indicating that the iteration should stop
+	stop chan<- int
 }
 
+// readCommitIter reads the commits from `iter`, generates corresponding entries and places them in the channel `next`.
+// If a value is read from `stop`, the function returns immediately.
 func readCommitIter(iter object.CommitIter, next chan<- *fuse.DirEntry, stop <-chan int) {
 	funcName := logging.CurrentFuncName(0, logging.Package)
 	err := iter.ForEach(func(commit *object.Commit) error {
@@ -102,6 +120,7 @@ func readCommitIter(iter object.CommitIter, next chan<- *fuse.DirEntry, stop <-c
 	close(next)
 }
 
+// newCommitDirStream creates a new commitDirStream from the commit iterator and an optional HEAD symlink node.
 func newCommitDirStream(iter object.CommitIter, headLink *fs.Inode) *commitDirStream {
 	rest := make(chan *fuse.DirEntry, 5)
 	stop := make(chan int, 1)
@@ -110,6 +129,7 @@ func newCommitDirStream(iter object.CommitIter, headLink *fs.Inode) *commitDirSt
 	return ds
 }
 
+// HasNext returns true if there are more entries.
 func (s *commitDirStream) HasNext() bool {
 	logging.LogCall(logging.NilCtx{}, logging.CallCtx{})
 	if s.next == nil {
@@ -118,6 +138,7 @@ func (s *commitDirStream) HasNext() bool {
 	return s.next != nil || s.headLink != nil
 }
 
+// Next returns the next entry. Note that this function depends on HasNext being called first.
 func (s *commitDirStream) Next() (entry fuse.DirEntry, errno syscall.Errno) {
 	logging.LogCall(logging.NilCtx{}, logging.CallCtx{})
 	if s.headLink != nil {
@@ -136,6 +157,7 @@ func (s *commitDirStream) Next() (entry fuse.DirEntry, errno syscall.Errno) {
 	return
 }
 
+// Close closes the stream and cleans up any resources.
 func (s *commitDirStream) Close() {
 	logging.LogCall(logging.NilCtx{}, logging.CallCtx{})
 	s.next = nil
@@ -143,6 +165,9 @@ func (s *commitDirStream) Close() {
 	s.stop <- 1
 }
 
+// Readdir returns the contents of the directory representing all commits,
+// i.e. the HEAD symlink and the directories representing commits.
+// The result is based on the current state of the repository.
 func (n *allCommitsNode) Readdir(ctx context.Context) (fs.DirStream, syscall.Errno) {
 	logging.LogCall(n, nil)
 	iter, err := n.repo.CommitObjects()
@@ -153,6 +178,8 @@ func (n *allCommitsNode) Readdir(ctx context.Context) (fs.DirStream, syscall.Err
 	return newCommitDirStream(iter, n.getHeadLinkNode(ctx)), fs.OK
 }
 
+// Lookup returns a node representing the commit with the given hash, or the HEAD symlink if `name == "HEAD"`.
+// The result is based on the current state of the repository.
 func (n *allCommitsNode) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
 	logging.LogCall(n, logging.CallCtx{"name": name})
 	var err error
@@ -186,6 +213,7 @@ func (n *allCommitsNode) Lookup(ctx context.Context, name string, out *fuse.Entr
 	return node, fs.OK
 }
 
+// Getattr returns attributes corresponding to the current HEAD commit.
 func (n *allCommitsNode) Getattr(_ context.Context, _ fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
 	logging.LogCall(n, nil)
 	attr, err := headAttr(n)
