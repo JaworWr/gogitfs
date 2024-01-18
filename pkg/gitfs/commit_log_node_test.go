2fba9e02c86c3e64a339b8df35828fdec74de2a6
Michał Jaworski <jawor403@gmail.com>
2024-01-18 23:55:10 +0100

    Refactor commit symlink and basepath tests

--- pkg/gitfs/commit_log_node_test.go
+++ pkg/gitfs/commit_log_node_test.go
@@ -20,12 +20,12 @@ func Test_commitSymlink(t *testing.T) {
 		expectedPrefix string
 	}{
 		{
-			"foo without prefix",
+			"symlink without prefix",
 			args{"foo", nil},
 			"",
 		},
 		{
-			"bar with prefix",
+			"symlink with prefix",
 			args{"bar", &basePath},
 			"asdf/",
 		},
@@ -40,45 +40,38 @@ func Test_commitSymlink(t *testing.T) {
 			node := commitSymlink(commit, tc.basePath)
 
 			p := string(node.Data)
-			assert.Equal(t, tc.expectedPrefix+hash.String(), p, "incorrect path")
+			assert.Equal(t, tc.expectedPrefix+hash.String(), p, "incorrect symlink path")
 
 			commitTime := commitSignatures[tc.commit].When.Unix()
-			assert.EqualValues(t, commitTime, node.Attr.Mtime, "incorrect mtime")
-			assert.EqualValues(t, commitTime, node.Attr.Atime, "incorrect atime")
-			assert.EqualValues(t, commitTime, node.Attr.Ctime, "incorrect ctime")
+			assert.EqualValues(t, commitTime, node.Attr.Mtime, "incorrect symlink mtime")
+			assert.EqualValues(t, commitTime, node.Attr.Atime, "incorrect symlink atime")
+			assert.EqualValues(t, commitTime, node.Attr.Ctime, "incorrect symlink ctime")
 		})
 	}
 }
 
 func Test_getBasePath(t *testing.T) {
+	expectedBasePath := "../.."
 	tests := []struct {
 		name       string
 		linkLevels int
-		expectNil  bool
-		expected   string
+		expected   *string
 	}{
 		{
 			"zero",
 			0,
-			true,
-			"",
+			nil,
 		},
 		{
 			"non-zero",
 			2,
-			false,
-			"../..",
+			&expectedBasePath,
 		},
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			basePath := getBasePath(tc.linkLevels)
-			if tc.expectNil {
-				assert.Nil(t, basePath)
-			} else {
-				assert.NotNil(t, basePath)
-				assert.Equal(t, tc.expected, *basePath)
-			}
+			assert.Equal(t, tc.expected, basePath, "incorrect basepath")
 		})
 	}
 }
