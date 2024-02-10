1fd44523153655d023841e10d076fa514e59b6c5
Michał Jaworski <jawor403@gmail.com>
2024-02-10 23:01:58 +0100

    Document environment.go

--- pkg/gitfs/environment.go
+++ pkg/gitfs/environment.go
@@ -2,11 +2,19 @@ package gitfs
 
 import "gogitfs/pkg/inode_manager"
 
+// commitCache is an InodeCache storing all commit nodes. This allows us to avoid duplication of commitNode objects.
 var commitCache *inode_manager.InodeCache
+
+// branchCache is a branchNodeCache storing all branch nodes and updating them as needed.
 var branchCache *branchNodeCache
+
+// initRun tells us whether Init() has been called.
 var initRun = false
 
+// commitIno is the initial inode number for the commit nodes.
 var commitIno uint64 = 2 << 60
+
+// branchIno is the initial inode number for the branch nodes.
 var branchIno uint64 = 2 << 59
 
 func Init() {
