e3ed400be155ebe381ffc1c54b56d702f14661d9
Michał Jaworski <jawor403@gmail.com>
2024-04-02 20:45:47 +0200

    Implement dirNode.Getattr

--- pkg/gitfs/dir_node.go
+++ pkg/gitfs/dir_node.go
@@ -1,10 +1,13 @@
 package gitfs
 
 import (
+	"context"
 	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/hanwen/go-fuse/v2/fs"
 	"github.com/hanwen/go-fuse/v2/fuse"
 	"gogitfs/pkg/gitfs/internal/utils"
 	"gogitfs/pkg/logging"
+	"syscall"
 )
 
 type dirNode struct {
@@ -19,3 +22,11 @@ func (n *dirNode) GetCallCtx() logging.CallCtx {
 	info["mode"] = n.entry.Mode
 	return info
 }
+
+func (n *dirNode) Getattr(_ context.Context, _ fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
+	logging.LogCall(n, nil)
+	out.Attr = n.attr
+	return fs.OK
+}
+
+var _ fs.NodeGetattrer = (*dirNode)(nil)
